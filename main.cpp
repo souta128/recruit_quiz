b3bc31e34493d4a2d952796085d5e1ad471ae34c
颯大 表 <biaosada10@outlook.com>
2024-10-02 16:11:12 +0900

    問題の数値をランダムに

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include<iostream>
 #include<string>
+#include<random>
 using namespace std;
 
 //問題文と答えをまとめる構造体
 struct Question
 {
-	string s;
+	string q;
 	int a;
 };
 int main()
 {
-	Question questions[] = {
-		{"13*(-5)",13 * -5},
-		{"(-21) / (-3)",-21 / -3},
-		{"7-(4+2)/2",7 - (4 + 2) / 2},
-	};
+	Question questions[3];
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	//掛け算
+	int x = uniform_int_distribution<>(1,30)(rand);
+	int y = uniform_int_distribution<>(1,20)(rand);
+	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].a = x * y;
+
+	//割り算
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[1].q = to_string(x) + " / " + to_string(y);
+	questions[1].a = x / y;
+	//複雑な式
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	int z = uniform_int_distribution<>(1, 10)(rand);
+	int w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q =
+		to_string(x) + "-(" + to_string(y) + "+" + to_string(z) + ")/" + to_string(w);
+	questions[2].a = x - (y + z) / w;
+
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions) {
-		cout << e.s << "の答えは？\n";
+		cout << e.q << "の答えは？\n";
 		int answer;
 		cin >> answer;
 		if (answer ==e.a) {
