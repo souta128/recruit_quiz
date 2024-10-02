5341fb7257945201619e16d1d1410f7a3fedbb80
颯大 表 <biaosada10@outlook.com>
2024-10-02 15:06:18 +0900

    問題数を増やす

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+//問題文と答えをまとめる構造体
+struct Question
+{
+	string s;
+	int a;
+};
 int main()
 {
+	Question questions[] = {
+		{"13*(-5)",13 * -5},
+		{"(-21) / (-3)",-21 / -3},
+		{"7-(4+2)/2",7 - (4 + 2) / 2},
+	};
 	cout << "[リクルート試験対策クイズ]\n";
-	cout << "13x(-5)の答えは？\n";
 
-	int answer;
-	cin >> answer;
-	if (answer == 13 * -5) {
+	for (const auto& e : questions) {
+		cout << e.s << "の答えは？\n";
+		int answer;
+		cin >> answer;
+		if (answer ==e.a) {
 		cout << "正解\n";
+		}
+		else {
+		cout << "間違い！正解は" << e.a << "\n";
+		}//for questions
 	}
-	else {
-		cout << "間違い！正解は" << 13 * -5 << "\n";
-	}
+	
+	
 }
