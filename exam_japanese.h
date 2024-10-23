f0ebb813f3376434131f2a5d43928155ff22e2cc
颯大 表 <biaosada10@outlook.com>
2024-10-23 16:44:43 +0900

    似た意味を持つ漢字の問題を出題できるように追加

--- exam_japanese.h
+++ exam_japanese.h
@@ -12,3 +12,6 @@ QuestionList CreateHomophoneExam();
 
 // ‘Î‹`Œê‚Ì–â‘è‚ğì¬‚·‚é
 QuestionList CreateAntonymExam();
+
+// —Ş‹`Œê‚Ì–â‘è‚ğì¬‚·‚é
+QuestionList CreateSynonymExam();
--- main.cpp
+++ main.cpp
@@ -117,6 +117,8 @@ int main()
 		questions.insert(questions.end(), homophoneExam.begin(), homophoneExam.end());
 		QuestionList antonymExam = CreateAntonymExam();
 		questions.insert(questions.end(), antonymExam.begin(), antonymExam.end());
+		QuestionList synonymExam = CreateSynonymExam();
+		questions.insert(questions.end(), synonymExam.begin(), synonymExam.end());
 	}
 
 
