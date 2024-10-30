81283a103663379fac98c7c0efed114a3e465355
颯大 表 <biaosada10@outlook.com>
2024-10-30 14:54:05 +0900

    英語の問題を追加し、問題を出題できるようにした

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include"question.h"
 #include"exam_japanese.h"
+#include"exam_english.h"
 #include<iostream>
 #include<vector>
 #include<random>
@@ -106,7 +107,7 @@ int main()
 		to_string(z) });
 	cout << "[リクルート試験対策クイズ]\n";
 
-	cout << "教科を選んでください\n1=数学\n2=国語\n";
+	cout << "教科を選んでください\n1=数学\n2=国語\n3=英語\n";
 	int subject;
 	cin >> subject;
 	if (subject == 2) {
@@ -119,6 +120,9 @@ int main()
 		questions.insert(questions.end(), antonymExam.begin(), antonymExam.end());
 		QuestionList synonymExam = CreateSynonymExam();
 		questions.insert(questions.end(), synonymExam.begin(), synonymExam.end());
+	}
+	else if (subject == 3) {
+		questions = CreateEnglishWordExam();
 	}
 
 
