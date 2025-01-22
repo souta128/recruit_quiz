7825b529250cdde379d6ec2be5b185fbb296b174
颯大 表 <biaosada10@outlook.com>
2025-01-22 15:44:04 +0900

    ０を入力し、総合問題を出題できるようにした

--- main.cpp
+++ main.cpp
@@ -40,9 +40,9 @@ int main()
 	cout << "[リクルート試験対策クイズ]\n";
 
 	cout << "教科を選んでください\n";
+	cout << "0=総合テスト\n";
 	for (int i = 0; i < size(subjectData); i++) {
 		cout << i + 1 << '=' << subjectData[i].name << '\n';
-		
 	}	
 	
 	int subject;
@@ -50,8 +50,16 @@ int main()
 
 	if (subject > 0 && subject <= size(subjectData)) {
 		questions = subjectData[subject - 1].create();
-		
 	}
+	else if (subject == 0) {
+		// 総合テスト
+		questions.clear();
+		for (int i = 0; i < size(subjectData); i++) {
+			QuestionList tmp = subjectData[i].create();
+			questions.insert(questions.end(), tmp.begin(), tmp.end());
+		}
+	}
+
 		for (const auto& e : questions) {
 			cout << e.q << "\n";
 			string answer;
